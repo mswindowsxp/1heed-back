50cceb4de731fc6e09216c67b44d127f37bad2ac
dungvv <dungvv.vnu@gmail.com>
2019-05-24 23:27:44 +0700

    add swagger

--- src/main/java/io/uetunited/heed/config/WebMvcConfig.java
+++ src/main/java/io/uetunited/heed/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package io.uetunited.heed.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,4 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
