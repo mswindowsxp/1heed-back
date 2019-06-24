425470b026bc6fa325afd1e7e21d89b9619fb043
dungvv <dungvv@agiletech.vn>
2019-06-24 11:58:46 +0700

    add webhook controller

--- src/main/java/io/uetunited/oneheed/controller/MessengerController.java
+++ src/main/java/io/uetunited/oneheed/controller/MessengerController.java
@@ -1,22 +1,16 @@
 /**
- * 
+ *
  */
 package io.uetunited.oneheed.controller;
 
-import javax.validation.Valid;
-
+import io.uetunited.oneheed.payload.WebhookEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import io.uetunited.oneheed.payload.WebhookEvent;
-import lombok.extern.slf4j.Slf4j;
+import javax.validation.Valid;
 
 /**
  * @author la-pc
@@ -26,29 +20,29 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/webhook")
 @Slf4j
 public class MessengerController {
-	private static final String EVENT_RECEIVED = "EVENT_RECEIVED";
-	private static final String SUBSCRIBE = "subscribe";
-	@Value("${app.verify.token}")
+    private static final String EVENT_RECEIVED = "EVENT_RECEIVED";
+    private static final String SUBSCRIBE = "subscribe";
+    @Value("${app.verify.token}")
     String verify_token;
-	
-	@PostMapping
-	public ResponseEntity<?> postEvent(@Valid @RequestBody WebhookEvent webhookEvent) {
-		log.info("webhookEvent: {}", webhookEvent);
+
+    @PostMapping
+    public ResponseEntity<?> postEvent(@Valid @RequestBody WebhookEvent webhookEvent) {
+        log.info("webhookEvent: {}", webhookEvent);
         return ResponseEntity.ok(EVENT_RECEIVED);
     }
 
-	@GetMapping
-	public ResponseEntity verifyWebhook(	@RequestParam(value = "mode") String mode, 
-											@RequestParam(value = "verify_token") String verifyToken,
-											@RequestParam(value = "challenge") String challenge) {
-		if (StringUtils.isNotBlank(mode) && StringUtils.isNotBlank(verifyToken)) {
-			if (SUBSCRIBE.equalsIgnoreCase(mode) && verify_token.equalsIgnoreCase(verifyToken)) {
-				log.debug("WEBHOOK_VERIFIED");
-				return ResponseEntity.ok(challenge);
-			} else {
-				return ResponseEntity.status(403).build();
-			}
-		}
-		return ResponseEntity.status(403).build();
-	}
+    @GetMapping
+    public ResponseEntity verifyWebhook(@RequestParam(value = "mode") String mode,
+                                        @RequestParam(value = "verify_token") String verifyToken,
+                                        @RequestParam(value = "challenge") String challenge) {
+        if (StringUtils.isNotBlank(mode) && StringUtils.isNotBlank(verifyToken)) {
+            if (SUBSCRIBE.equalsIgnoreCase(mode) && verify_token.equalsIgnoreCase(verifyToken)) {
+                log.debug("WEBHOOK_VERIFIED");
+                return ResponseEntity.ok(challenge);
+            } else {
+                return ResponseEntity.status(403).build();
+            }
+        }
+        return ResponseEntity.status(403).build();
+    }
 }
