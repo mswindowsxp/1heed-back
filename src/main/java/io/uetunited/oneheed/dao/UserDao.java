f367d19220e0f79aa22e49d0e549ccabd4364a4d
dungvv <dungvv@agiletech.vn>
2019-06-27 16:09:15 +0700

    correct dao

--- src/main/java/io/uetunited/oneheed/dao/UserDao.java
+++ src/main/java/io/uetunited/oneheed/dao/UserDao.java
@@ -4,6 +4,7 @@ import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
 import io.uetunited.oneheed.dao.mapper.UserMapper;
 import io.uetunited.oneheed.payload.dto.User;
 import org.apache.commons.lang3.StringUtils;
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
@@ -12,34 +13,50 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
-public interface UserDao {
+public class UserDao {
 
     @Autowired
     Jdbi jdbi = null;
 
-    @SqlQuery("select * from users where id=:id limit 1")
-    @RegisterRowMapper(UserMapper.class)
-    User getById(@Bind("id") String id);
+    public Optional<User> getById(@Bind("id") String id) {
+        String sql = "select * from users where id=:id";
+        try (Handle handle = jdbi.open()) {
+            return handle.createQuery(sql).bind("id", id).mapToBean(User.class).findFirst();
+        }
+    }
 
-    @SqlQuery("select * from users where social_id=:socialId AND \"type\"=:type limit 1")
-    @RegisterRowMapper(UserMapper.class)
-    User getBySocialId(@Bind("socialId") String socialId, @Bind("type") String type);
+    public Optional<User> getBySocialId(@Bind("socialId") String socialId, @Bind("type") String type) {
+        String sql = "select * from users where social_id=:socialId AND \"type\"=:type";
+        try (Handle handle = jdbi.open()) {
+            return handle.createQuery(sql).bind("socialId", socialId).bind("type", type).mapToBean(User.class).findFirst();
+        }
+    }
 
-    @SqlQuery("select * from users where username=:username limit 1")
-    @RegisterRowMapper(UserMapper.class)
-    User getByUsername(@Bind("username") String username);
+    public Optional<User> getByUsername(@Bind("username") String username) {
+        String sql = "select * from users where username=:username limit 1";
+        try (Handle handle = jdbi.open()) {
+            return handle.createQuery(sql).bind("username", username).mapToBean(User.class).findFirst();
+        }
+    }
 
-    @SqlQuery("insert into users\n" +
-            "(id, created_at, updated_at, email, \"name\", \"type\", avatar, social_id, is_active, access_token, username)\n" +
-            "VALUES(:id, now(), now(), :email, :name, :type, :avatar, :socialId, true, :accessToken, :username) on conflict (username) do update set access_token = EXCLUDED.access_token;")
-    @RegisterRowMapper(UserMapper.class)
-    @GetGeneratedKeys
-    User createUser(@Bind("id") String id, @Bind("email") String email, @Bind("name") String name, @Bind("type") String type,
-                    @Bind("avatar") String avatar, @Bind("socialId") String socialId, @Bind("accessToken") String accessToken, @Bind("username") String username);
+    public Optional<User> createUser(@Bind("id") String id, @Bind("email") String email, @Bind("name") String name, @Bind("type") String type,
+                    @Bind("avatar") String avatar, @Bind("socialId") String socialId, @Bind("accessToken") String accessToken, @Bind("username") String username) {
+        String sql = "insert into users\n" +
+                "(id, created_at, updated_at, email, \"name\", \"type\", avatar, social_id, is_active, access_token, username)\n" +
+                "VALUES(:id, now(), now(), :email, :name, :type, :avatar, :socialId, true, :accessToken, :username) on conflict (username) do update set access_token = EXCLUDED.access_token";
+        try (Handle handle = jdbi.open()) {
+            return handle.createUpdate(sql)
+                    .bind("id", id).bind("email", email).bind("name", name).bind("avatar", avatar)
+                    .bind("socialId", socialId).bind("accessToken", accessToken).bind("username", username)
+                    .executeAndReturnGeneratedKeys().mapToBean(User.class).findFirst();
+        }
+    }
 
-    default User createOrUpdateUserAccessToken(User user) {
+    public Optional<User> createOrUpdateUserAccessToken(User user) {
         if (StringUtils.isBlank(user.getId())) {
             user.setId(NanoIdUtils.randomNanoId());
         }
--- src/main/java/io/uetunited/oneheed/dao/redis/RefreshTokenDao.java
+++ src/main/java/io/uetunited/oneheed/dao/redis/RefreshTokenDao.java
@@ -2,34 +2,37 @@ package io.uetunited.oneheed.dao.redis;
 
 import io.uetunited.oneheed.payload.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Repository
 @Transactional
 public class RefreshTokenDao {
-    private static final String KEY = "REFRESH_TOKEN";
+    private static final String PREFIX = "REFRESH_TOKEN_";
 
     @Autowired
     RedisTemplate<String, User> redis;
 
-    HashOperations<String, String, User> hashOps = redis.opsForHash();
 
     public void addRefreshToken(String refreshToken, User user) {
-        hashOps.put(KEY, refreshToken, user);
+        redis.opsForValue().set(PREFIX + refreshToken, user);
     }
 
     public User getUserFromRefreshToken(String refreshToken) {
-        return hashOps.get(KEY, refreshToken);
+        return redis.opsForValue().get(PREFIX + refreshToken);
     }
 
     public long deleteRefreshToken(String... refreshTokens) {
-        return hashOps.delete(KEY, refreshTokens);
+        List<String> toDel = Arrays.asList(refreshTokens).stream().map(s -> PREFIX + s).collect(Collectors.toList());
+        return redis.delete(toDel);
     }
 
     public boolean checkIfRefreshTokenExists(String refreshToken) {
-        return hashOps.get(KEY, refreshToken) != null;
+        return redis.hasKey(PREFIX + refreshToken);
     }
 }
