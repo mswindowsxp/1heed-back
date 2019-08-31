73e0460b1b4da591b09464c5f3ca3e2eef58fb9c
HaCT1 <hact1@fsoft.com.vn>
2019-08-31 14:27:19 +0700

    Update tag dao

--- src/main/java/io/uetunited/oneheed/dao/tag/TagDao.java
+++ src/main/java/io/uetunited/oneheed/dao/tag/TagDao.java
@@ -1,6 +1,7 @@
 package io.uetunited.oneheed.dao.tag;
 
 import io.uetunited.oneheed.model.facebook.Tag;
+import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,11 @@ public class TagDao {
     @Autowired
     Jdbi jdbi = null;
 
-    public Optional<List<Tag>> getListTag (String pageID) {}
+    public List<Tag> getListTag (String pageID) {
+        String sql = "select * from tag where pageID=:pageID";
+        try(Handle handle = jdbi.open()) {
+            return handle.createQuery(sql).bind("pageID", pageID).mapToBean(Tag.class).list();
+        }
+
+    }
 }
